5fdb62ba89a7939954ac707d4411cf9ff66d24e4
Small-Lexy <92353062+Small-Lexy@users.noreply.github.com>
2022-10-26 14:52:37 -0300

    Update routes.js

--- totem/routes.js
+++ totem/routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-
+const { transporter } = require('./Api/mainApi.js')
 const { justPonto, justFerias } = require('./models.js')
 
 
@@ -11,7 +11,7 @@ router.post("/", async (req, res) => {
 
   //req body
 
-  const { matriculaPonto, nomePonto, areaAtuacao, setorPonto, dataJust, motivoPonto, entrada, saida, descricao, hExtras, observacao, aprovado } = req.body
+  const { matriculaPonto, nomePonto, areaAtuacao, setorPonto, dataJust, motivoPonto, entrada, saida, descricao, hExtras, observacao,  aprovado, emailPonto } = req.body
 
   const justPonto2 = {
     matriculaPonto,
@@ -25,8 +25,36 @@ router.post("/", async (req, res) => {
     descricao,
     hExtras,
     observacao,
-    aprovado
+    aprovado,
+    emailPonto
   }
+
+  transporter.sendMail({
+    from: "Totem <testeluwroblewski@gmail.com>",
+    to: `${emailPonto}`,
+subject: "Solicitação colaborador",
+html: `<div style= "border: 4px; border-style: solid; border-color: rgb(93, 85, 133); width: 500px; height: 630px; background-color: rgb(127, 117, 179)" >
+<h2 style="font-family: roboto-condesed; color: rgb(255, 255, 255); text-align: center;" >Justificativa de ponto</h2>
+<p style="font-family: roboto-condesed; color: rgb(255, 255, 255);  text-align: center; font-size: large;"  >Matricula: ${matriculaPonto}</p>
+<p style="font-family: roboto-condesed; color: rgb(255, 255, 255);  text-align: center; font-size: large;"  >Nome do colaborador: ${nomePonto}</p>
+<p style="font-family: roboto-condesed; color: rgb(255, 255, 255);  text-align: center; font-size: large;"  >Area de atuação: ${areaAtuacao}</p>
+<p style="font-family: roboto-condesed; color: rgb(255, 255, 255);  text-align: center; font-size: large;"  >Setor: ${setorPonto}</p>
+<p style="font-family: roboto-condesed; color: rgb(255, 255, 255);  text-align: center; font-size: large;"  >Data da justificativa: ${dataJust}</p>
+<p style="font-family: roboto-condesed; color: rgb(255, 255, 255);  text-align: center; font-size: large;"  >Motivo da solicitação: ${motivoPonto}</p>
+<p style="font-family: roboto-condesed; color: rgb(255, 255, 255);  text-align: center; font-size: large;"  >Entrada: ${entrada}</p>
+<p style="font-family: roboto-condesed; color: rgb(255, 255, 255);  text-align: center; font-size: large;"  >Saida: ${saida}</p>
+<p style="font-family: roboto-condesed; color: rgb(255, 255, 255);  text-align: center; font-size: large;"  >Descrição do motivo: ${descricao}</p>
+<p style="font-family: roboto-condesed; color: rgb(255, 255, 255);  text-align: center; font-size: large;"  >Horas extras: ${entrada}</p>
+<p style="font-family: roboto-condesed; color: rgb(255, 255, 255);  text-align: center; font-size: large;"  >Observações: ${observacao}</p>
+<p style="font-family: roboto-condesed; color: rgb(255, 255, 255);  text-align: center; font-size: large;"  >Email usado: ${emailPonto}</p>
+<p style="font-family: Arial; color: rgb(255, 255, 255);  text-align: center;"  ><small>Se voce não fez a solicitação por favor ignorar.</small></p>
+</div> `
+
+}).then(message =>{
+console.log(message)
+}).catch(err =>{
+    console.log(err)
+})
 
   try {
     await justPonto.create(justPonto2)
@@ -37,6 +65,7 @@ router.post("/", async (req, res) => {
   }
 
 });
+
 
 
 
